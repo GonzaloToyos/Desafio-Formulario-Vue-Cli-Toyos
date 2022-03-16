bb57e48686201bc77ea36901a08bb9b73f5ad29f
Gonzalo Toyos <gonzalotoyos2001@gmail.com>
2022-03-15 22:20:06 -0300

    Subiendo desafio

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form-vue />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FormVue from "./components/FormVue.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    FormVue,
+  },
+};
 </script>
 
 <style>
